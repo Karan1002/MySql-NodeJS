826e7931d9c6eb585856cbc490cb466dcdf64666
Karan1002 <karankushwaha7227@gmail.com>
2024-02-15 10:48:32 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_USER_HOST,
+  user: process.env.MYSQL_USER_NAME,
+  password: process.env.MYSQL_USER_PASSWORD,
+  database: process.env.MYSQL_USER_DB,
+});
+
+try {
+  connection.connect();
+  console.log("connected");
+} catch (error) {
+  console.log(error);
+}
+
+try {
+  connection.query("SELECT * FROM users", (error, result) => {
+    console.log(result);
+  });
+} catch (error) {
+  console.log(error);
+}
